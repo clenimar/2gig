912a0a8cad20683b7695f5aee9972e6123a6d8f4
Clenimar <clenimar.filemon@gmail.com>
2015-11-03 23:02:34 -0300

    Home layout

--- public/javascripts/controllers/new.js
+++ public/javascripts/controllers/new.js
@@ -3,30 +3,16 @@
 
     angular
         .module('2GIG')
-        .controller('NewAdCtrl', function($scope, $http) {
-            $scope.new = {
-                "author":"",
-                "title":"",
-                "description":"",
-                "street":"",
-                "number":"",
-                "neighbourhood":"",
-                "city":"",
-                "state":"",
-                "country":"",
-                "email":"",
-                "phone1":"",
-                "instrument":"",
-                "desired_styles":"",
-                "undesired_styles":"",
-                "interest":"",
-                "passwd":""
-            };
+        .controller('NewAdCtrl', function($scope, $http, $window) {
+            $scope.new = {};
 
             $scope.newAds = function() {
                 $http.post('/api/ads', $scope.new)
                     .success(function() {
+                        Materialize.toast("Anúncio criado com sucesso. Agora é só esperar!", 5000);
                         console.log("deu certo, moral!");
+                        $scope.new = {};
+                        $window.location = '/';
                     })
                     .error(function() {
                         console.log("error while trying to post content...");
